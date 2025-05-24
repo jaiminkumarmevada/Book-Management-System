63c2805136c165a28c20187c924194b36ce860fb
Jaimin Mevada <jaiminkumarmevada@gmail.com>
2025-05-24 13:53:59 +0530

    Add project files.

---
+++ BookManager/Controllers/BookController.cs
@@ -0,0 +1,53 @@
+﻿using BookManager.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookManager.Controllers
+{
+    public class BookController : Controller
+    {
+        BookManagerDBEntities dbObj= new BookManagerDBEntities();
+
+
+        // GET: Book
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddBook(Book Model)
+        {
+
+            if (ModelState.IsValid)
+            {
+                Book obj = new Book();
+                // obj.ID = Model.ID;
+                obj.Title = Model.Title;
+                obj.Author = Model.Author;
+                obj.Price = Model.Price;
+                obj.PublishedDate = Model.PublishedDate;
+                dbObj.Books.Add(obj);
+                dbObj.SaveChanges();
+            }
+            ModelState.Clear();
+            return View("Index");
+        }
+
+
+        public ActionResult BookList()
+        {
+            var res = dbObj.Books.ToList();
+            return View(res);
+        }
+        public ActionResult Delete(int ID)
+        {
+            var res = dbObj.Books.Where(model => model.ID == ID).First();
+            return View(res);
+        }
+
+    }
+}
