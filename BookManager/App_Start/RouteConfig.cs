6989ed9c0d4b21b4c873a13c44489f97585e5ba4
Jaimin Mevada <jaiminkumarmevada@gmail.com>
2025-05-24 22:48:42 +0530

    Rount Config File Updated

--- BookManager/App_Start/RouteConfig.cs
+++ BookManager/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace BookManager
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Book", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Book", action = "BookList", id = UrlParameter.Optional }
             );
         }
     }
